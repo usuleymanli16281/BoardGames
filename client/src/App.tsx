1b0973be776aa6db2bd362904cb0a9dd6b9a6520
samxal <sh.samxal@gmail.com>
2023-12-14 20:09:07 +0400

    merged bracnhes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Navigate, Route, Routes } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Login from './Components/Login'
 import Register from './Components/Register'
 import GamesContainer from './Components/GamesContainer'
---
+++ server/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using BoardGames.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Player> Players { get; set; }
+}
