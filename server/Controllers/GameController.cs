9150e00195467e8efff7168050b8582c92381041
Umid <suleymanli4240@gmail.com>
2023-12-18 14:58:48 +0400

    Update

---
+++ server/Controllers/GameController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BoardGames.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IConfiguration _configuration;
+
+        public GameController(ApplicationDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+        [HttpGet("/games/{name}")]
+        public async Task<IActionResult> GetName(string name)
+        {
+            if(name != null)
+            {
+                return Ok(name);
+            }
+            return BadRequest();
+        }
+    }
+}
